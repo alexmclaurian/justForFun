4f5f29189efb2f92360f7dbb28a65b11234082b3
alex <jiraiya.sama@gmail.com>
2020-11-27 17:14:00 -0600

    basic content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just For Fun
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        button redirect to generate content page
+        {/* 
+        after button is clicked, go to new page
+        */}
+        <a href="">redirect</a>
       </header>
     </div>
   );
